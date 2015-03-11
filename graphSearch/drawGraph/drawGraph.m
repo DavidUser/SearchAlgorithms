1b32651ae1ca55eaaddba8099fbdeb04077fc30e
David Kennedy S. A <daviduser.guj@gmail.com>
2015-03-11 03:58:53 -0300

    draw graphs with scale, default scale one

--- graphSearch/drawGraph/drawGraph.m
+++ graphSearch/drawGraph/drawGraph.m
@@ -1,7 +1,7 @@
-function graphFigure = drawGraph(states, transitions)
+function graphFigure = drawGraph(states, transitions, scale = 1)
 	graphFigure = drawBackgroundGraph(max(states(:,1)), max(states(:,2)));
 	for i=1:size(states)(1)
-		drawNodeGraph(states(i,1), states(i,2), i);
+		drawNodeGraph(states(i,1), states(i,2), i, scale);
 	end
 	for i=1:size(transitions)(1)
 		for j=1:size(transitions)(2)
--- graphSearch/drawGraph/drawNodeGraph.m
+++ graphSearch/drawGraph/drawNodeGraph.m
@@ -1,6 +1,7 @@
-function drawNodeGraph(posX, posY, name)
+function drawNodeGraph(posX, posY, name, scale = 1)
 	hold on;
-	[X Y Z] = ellipsoid(posX, posY, 0, .3, .3, 0);
+	radius = .3*scale;
+	[X Y Z] = ellipsoid(posX, posY, 0, radius, radius, 0);
 	surf(X,Y,Z,'EdgeColor', 'none');
 	if (isnumeric(name))
 		text(posX, posY, int2str(name));
