f6e8e88015853499bf5d164bc821999e1bd2bff3
David Kennedy S. A <daviduser.guj@gmail.com>
2015-03-12 17:23:37 -0300

    add resource transitions colored

--- graphSearch/drawGraph/drawArrowGraph.m
+++ graphSearch/drawGraph/drawArrowGraph.m
@@ -1,4 +1,4 @@
-function drawArrowGraph(initial, final, label = ' ', scale = 1)
+function drawArrowGraph(initial, final, label = ' ', scale = 1, color = 'black')
 	offset = .3*scale;
 	for i = 1:2
 		if (initial(i) < final(i))
@@ -12,12 +12,12 @@ function drawArrowGraph(initial, final, label = ' ', scale = 1)
 	end
 	projectionX = [initial(1) final(1)];
 	projectionY = [initial(2) final(2)];
-	line(projectionX, projectionY);
+	line(projectionX, projectionY, 'color', color);
 
 	if (isnumeric(label))
 		finalLabel = num2str(label);
 	else
 		finalLabel = label;
 	end
-	text(mean(projectionX), mean(projectionY), finalLabel);
+	text(mean(projectionX), mean(projectionY), finalLabel, 'color', color);
 end
--- graphSearch/drawGraph/drawGraph.m
+++ graphSearch/drawGraph/drawGraph.m
@@ -9,7 +9,7 @@ function graphFigure = drawGraph(states, transitions, scale = 1)
 				if ( i > j && transitions(i,j) == transitions(j,i)) % no directional arrow
 					continue;
 				end
-				drawArrowGraph(states(i,:), states(j,:), transitions(i,j), scale);
+				drawArrowGraph(states(i,:), states(j,:), transitions(i,j), scale, rand(1,3));
 			end
 		end
 	end
