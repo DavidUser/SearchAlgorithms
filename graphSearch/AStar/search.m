f6a83ac36eaf0ccc2f4832b8e29f8a0b05d21f3e
David Kennedy S. A <daviduser.guj@gmail.com>
2015-03-13 17:16:28 -0300

    A* search with multiple goals, no tested

--- graphSearch/AStar/search.m
+++ graphSearch/AStar/search.m
@@ -1,16 +1,19 @@
-function [ ways, waysCost ] = search(graph, heuristic, start, goal)
+function [ ways, waysCost ] = search(graph, heuristic, start, GOALS)
 	ways = zeros(0);
 	waysCost = zeros(0);
 	waysCostHeuristic = zeros(0);
 
-	haveIncompleteWays = true;
+	haveAWay = 1;
 
 	ways = start;
 	waysCost = 0;
 	waysCostHeuristic = 0;
 	lowerWay = 1;
 
-	while ( haveIncompleteWays )
+	findElement = @(COLLECTION, element) nthargout(2, @ismember, element, COLLECTION);
+	findLowerElement = @(COLLECTION, element) findElement(COLLECTION, min(COLLECTION));
+
+	while ( haveAWay ) % have a lower way to expand
 		way = ways(lowerWay,:);
 		cost = waysCost(lowerWay);
 
@@ -23,31 +26,34 @@ function [ ways, waysCost ] = search(graph, heuristic, start, goal)
 		expansionCost = expansionCost + cost;
 		newWays = [ ones(numel(expansion),1) * way, expansion' ];
 
+		%repeat state to static ways
 		if (size(ways)(2) < size(newWays)(2))
 			ways = resize(ways, size(ways)(1), size(newWays)(2));
 			for i=1:size(ways)(1)
 				ways(i,find(ways(i,:) == 0)) = ways(i,find(ways(i,:)))(end);
 			end	
 		end
-		ways = [ways; newWays];
+		ways = [ways; newWays]
 		waysCost = [waysCost expansionCost(expansion)];
 
 		expansionCostHeuristic = expansionCost + heuristic;
 		expansionCostHeuristic = expansionCostHeuristic(expansion);
-		waysCostHeuristic = [waysCostHeuristic expansionCostHeuristic];
+		waysCostHeuristic = [waysCostHeuristic expansionCostHeuristic]
 		
 		rankWay = waysCostHeuristic - min(waysCostHeuristic);
-		lowerWay = find(not(rankWay));
-		lowerWay = lowerWay(1);
+		lowerWay = findLowerElement(rankWay);
+		haveAWay = finite(rankWay(lowerWay));
 		
-		while ( ways(lowerWay, end) == goal || ways(lowerWay, end) == 0  )
+		% find a new lower way
+		while ( haveAWay && not(any(graph(ways(lowerWay,end), :))) ) % lower way is cornered
+			if ( not(findElement(GOALS, ways(lowerWay, end))) )
+				ways(lowerWay, :) = [];
+				waysCost(lowerWay) = [];
+				waysCostHeuristic(lowerWay) = [];
+			end
 			rankWay(lowerWay) = inf;
-			if ( sum(finite(rankWay)) == 0) 
-				haveIncompleteWays = false;
-				break; % all ways founded
-			end;
-			lowerWay = find(not(rankWay - min(rankWay))); %find a new lower
-			lowerWay = lowerWay(1);
+			lowerWay = findLowerElement(rankWay)
+			haveAWay = finite(rankWay(lowerWay));
 		end
 
 	end
