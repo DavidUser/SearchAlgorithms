162d7a2aa86eb0541ac18a9f6283b591d7f85656
David Kennedy S. A <daviduser.guj@gmail.com>
2015-03-18 08:36:17 -0300

    test with float type transitions

--- graphSearch/AStar/test.m
+++ graphSearch/AStar/test.m
@@ -7,21 +7,21 @@ positions(4,:) = [3 9];
 positions(5,:) = [7 10];
 
 graph = zeros(5);
-graph(1,2) = 1;
-graph(1,3) = 4;
-graph(2,3) = 2;
-graph(2,4) = 5;
-graph(2,5) = 12;
-graph(3,4) = 2;
-graph(4,5) = 3;
+graph(1,2) = 1.5;
+graph(1,3) = 4.003;
+graph(2,3) = 2.1;
+graph(2,4) = 5.6;
+graph(2,5) = 12.98;
+graph(3,4) = 2.005;
+graph(4,5) = 3.07;
 graph(2,1) = graph(1,2);
 
 heuristic = zeros(0);
-heuristic(1) = 7;
-heuristic(2) = 6;
-heuristic(3) = 2;
-heuristic(4) = 1;
-heuristic(5) = 0;
+heuristic(1) = 7.1;
+heuristic(2) = 6.2;
+heuristic(3) = 2.3;
+heuristic(4) = 1.4;
+heuristic(5) = 0.5;
 
 start = 1;
 goal = 5;
